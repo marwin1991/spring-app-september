ebef412fb9d75890c99e28652664b022fb4ddb74
Peter <peter.zmilczak@gmail.com>
2022-07-02 13:57:41 +0200

    init project

---
+++ src/main/java/com/codecool/september/secuirtyapp/SecuirtyAppApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.september.secuirtyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuirtyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuirtyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/september/secuirtyapp/SecuirtyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.september.secuirtyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuirtyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
